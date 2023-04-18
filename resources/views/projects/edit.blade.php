c77c2ecbbcacad23d79c3df5d2ff5b9823066c6b
aiceey <nicetteocampo0911@gmail.com>
2023-04-18 09:45:57 +0800

    template4

---
+++ resources/views/projects/edit.blade.php
@@ -0,0 +1,32 @@
+@extends('layout')
+
+@section('content')
+	<h1>Edit Project</h1>
+
+	<form method="POST" action="/projects/{{ $project->id }}">
+
+		{{ method_field('PATCH') }} <!-- HIDDEN patch -->
+		{{ csrf_field() }}
+		<div class="field">
+			<label class="label">Title</label>
+
+			<div>
+				<input class="form-control" type="text" placeholder="Title" name="title" value="{{ $project->title }}">
+			</div>
+		</div>
+
+		<div class="field">
+			<label class="label">Description</label>
+
+			<div >
+				<textarea type="text" name="description" class="form-control" > {{ $project->description }}</textarea>
+			</div>
+		</div>
+		<br>
+		<div class="row">
+			<div>
+				<button type="submit" class="btn btn-primary btn-block btn-lg"> Update Project</button>
+			</div>
+		</div>
+	</form>
+@endsection
